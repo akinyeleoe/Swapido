5ad4ee8468790f6fc445ccc0acd993a2c4011c90
Emmanuel Akinyele <akinyeleoe@gmail.com>
2022-05-21 22:37:08 +0400

    Update EthSwap.test.js
    Adding choi testing

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,6 +1,21 @@
-//const Token = artifacts.require('Token');
+const { assert } = require('chai');
+const { Item } = require('react-bootstrap/lib/Breadcrumb');
+
+const Token = artifacts.require('Token');
 const EthSwap = artifacts.require("EthSwap");
 
-contracts('EthSwap', (accounts)=>{
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+    
+contract('EthSwap', (accounts)=>{
+
+    describe('EthSwap deployment', async() =>{
+        it('contract has a name', async()=>{
+            let ethSwap = await EthSwap.new()
+            const name = await ethSwap.name()
+            assert.equal(name, 'EthSwap Instant Exchange')
+        })
+    } )
 
 })
