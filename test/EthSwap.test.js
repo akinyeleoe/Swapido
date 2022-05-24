6696c15be4f2f473e9e0967f5c6f782f272d2099
Emmanuel Akinyele <akinyeleoe@gmail.com>
2022-05-24 22:58:14 +0400

    Update EthSwap.test.js

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -10,9 +10,17 @@ require('chai')
     
 contract('EthSwap', (accounts)=>{
 
+    let token, ethSwap
+
+    before(async() =>{
+        token = async Token.new()
+        ethSwap = await EthSwap.new()
+        //Transfr Token to Ethswap
+        await token.transfer(ethSwap.address, tokens('1000000'))
+    })
+
     describe('Token deployment', async() =>{
         it('Token has a name', async()=>{
-            let token = await Token.new()
             const name = await token.name()
             assert.equal(name, 'DApp Token')
         })
@@ -20,18 +28,13 @@ contract('EthSwap', (accounts)=>{
 
     describe('EthSwap deployment', async() =>{
         it('contract has a name', async()=>{
-            let ethSwap = await EthSwap.new()
             const name = await ethSwap.name()
             assert.equal(name, 'EthSwap Instant Exchange')
         })
+
         it('contract has tokens', async()=>{
-            let token = await Token.new()
-            let ethSwap = await EthSwap.new()
-            await token.transfer(ethSwap.address, '1000000000000000000000000')
             let balance = await token.balanceOf(ethSwap.address)
-            assert.equal(balance.toString(), '1000000000000000000000000')
+            assert.equal(balance.toString(), '1000000')
         })
     } )
-
-
 })
