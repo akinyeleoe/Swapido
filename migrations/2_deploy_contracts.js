7b9983c3f255d19334be956d371a28c9b714f4a9
Emmanuel Akinyele <akinyeleoe@gmail.com>
2022-05-24 12:05:15 +0400

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,18 @@
 const Token = artifacts.require('Token')
 const EthSwap = artifacts.require("EthSwap");
 
+module.exports = async function(deployer){
+  //Deploy Token
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+
+  //Deploy Ethswap
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed()
+
+  //Transfer all token to Ethswap(1 million)
+  await token.transfer(ethSwap.address, '100000000000000000')
+}
 
 module.exports = (deployer) =>{
   deployer.deploy(EthSwap);
